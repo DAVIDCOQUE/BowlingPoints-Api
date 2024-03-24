1b00cfae4f55eb323e1e550957cdf350abbac441
JohnSP <johnj262000@hotmail.com>
2024-03-24 15:50:10 -0500

    #add: new project from spring initializr

---
+++ src/main/java/com/bowlingpoints/BowlingPointsApplication.java
@@ -0,0 +1,13 @@
+package com.bowlingpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bowlingpoints/BowlingPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowlingpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
