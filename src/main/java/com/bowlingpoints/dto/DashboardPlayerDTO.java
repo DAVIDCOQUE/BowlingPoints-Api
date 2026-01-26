076e39d1abec6aa3485b65f129ebe0106cefb5e8
John Soto <johnj262000@hotmail.com>
2026-01-25 22:53:06 -0500

    #feat: ordenamiento de ranking general por resultados

--- src/main/java/com/bowlingpoints/dto/DashboardPlayerDTO.java
+++ src/main/java/com/bowlingpoints/dto/DashboardPlayerDTO.java
@@ -8,6 +8,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class DashboardPlayerDTO {
+    private Integer position;
     private Integer personId;
     private String fullName;
     private Double averageScore;
